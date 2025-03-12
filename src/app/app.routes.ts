3b90ec18e3528a01a79d9894f32764c3c0f614b1
ahmednule <nuleahmed6@gmail.com>
2025-03-12 22:10:35 +0300

    feature: crated the routes of the application then went ahead and crated links making it easy for one to be able to nagivate to the about and back

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
-        path:"/about", component:AboutComponent
+        path:'', component: AppComponent
     },
     {
-        path:"/contact", component:ContactComponent
+        path:"about", component:AboutComponent
+    },
+    {
+        path:"contact", component:ContactComponent
     }
 ];
